f9ac57a8dc30dc687952eab54e75594ebd83213f
CAVALCANTE NOA p2107540 <noa.cavalcante@etu.univ-lyon1.fr>
2024-01-19 11:07:08 +0100

    Setup routage

--- lib/navigation/app_router.dart
+++ lib/navigation/app_router.dart
@@ -1,4 +1,13 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:hue_dada/auth/view/auth_router.dart';
+import 'package:hue_dada/auth/view/login_page.dart';
+import 'package:hue_dada/auth/view/register_page.dart';
+import 'package:hue_dada/home/view/create_home_page.dart';
+import 'package:hue_dada/home/view/home_page.dart';
+import 'package:hue_dada/home/view/home_router.dart';
+import 'package:hue_dada/room/view/create_light_page.dart';
+import 'package:hue_dada/room/view/create_room_page.dart';
+import 'package:hue_dada/room/view/room_page.dart';
 
 part 'app_router.gr.dart';
 
@@ -7,8 +16,47 @@ class AppRouter extends _$AppRouter {
   @override
   List<AutoRoute> get routes => [
         AutoRoute(
-          path: '/',
-          page: HomeRoute.page,
+          path: '/home',
+          page: HomeRouterRoute.page,
+          children: [
+            AutoRoute(
+              initial: true,
+              path: '',
+              page: HomeRoute.page,
+            ),
+            AutoRoute(
+              path: 'create-home',
+              page: CreateHomeRoute.page,
+            ),
+            AutoRoute(
+              path: 'room',
+              page: RoomRoute.page,
+            ),
+            AutoRoute(
+              path: 'create-room',
+              page: CreateRoomRoute.page,
+            ),
+            AutoRoute(
+              path: 'create-light',
+              page: CreateLightRoute.page,
+            ),
+          ],
+        ),
+        AutoRoute(
+          initial: true,
+          path: '/auth',
+          page: AuthRouterRoute.page,
+          children: [
+            AutoRoute(
+              initial: true,
+              path: 'login',
+              page: LoginRoute.page,
+            ),
+            AutoRoute(
+              path: 'register',
+              page: RegisterRoute.page,
+            ),
+          ],
         ),
       ];
 }
--- lib/navigation/app_router.gr.dart
+++ lib/navigation/app_router.gr.dart
@@ -15,13 +15,117 @@ abstract class _$AppRouter extends RootStackRouter {
 
   @override
   final Map<String, PageFactory> pagesMap = {
+    AuthRouterRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const AuthRouterPage(),
+      );
+    },
+    CreateHomeRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const CreateHomePage(),
+      );
+    },
+    CreateLightRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const CreateLightPage(),
+      );
+    },
+    CreateRoomRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const CreateRoomPage(),
+      );
+    },
     HomeRoute.name: (routeData) {
       return AutoRoutePage<dynamic>(
         routeData: routeData,
         child: const HomePage(),
       );
-    }
+    },
+    HomeRouterRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const HomeRouterPage(),
+      );
+    },
+    LoginRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const LoginPage(),
+      );
+    },
+    RegisterRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const RegisterPage(),
+      );
+    },
+    RoomRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const RoomPage(),
+      );
+    },
   };
+}
+
+/// generated route for
+/// [AuthRouterPage]
+class AuthRouterRoute extends PageRouteInfo<void> {
+  const AuthRouterRoute({List<PageRouteInfo>? children})
+      : super(
+          AuthRouterRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'AuthRouterRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [CreateHomePage]
+class CreateHomeRoute extends PageRouteInfo<void> {
+  const CreateHomeRoute({List<PageRouteInfo>? children})
+      : super(
+          CreateHomeRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'CreateHomeRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [CreateLightPage]
+class CreateLightRoute extends PageRouteInfo<void> {
+  const CreateLightRoute({List<PageRouteInfo>? children})
+      : super(
+          CreateLightRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'CreateLightRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [CreateRoomPage]
+class CreateRoomRoute extends PageRouteInfo<void> {
+  const CreateRoomRoute({List<PageRouteInfo>? children})
+      : super(
+          CreateRoomRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'CreateRoomRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
 }
 
 /// generated route for
@@ -34,6 +138,62 @@ class HomeRoute extends PageRouteInfo<void> {
         );
 
   static const String name = 'HomeRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [HomeRouterPage]
+class HomeRouterRoute extends PageRouteInfo<void> {
+  const HomeRouterRoute({List<PageRouteInfo>? children})
+      : super(
+          HomeRouterRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'HomeRouterRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [LoginPage]
+class LoginRoute extends PageRouteInfo<void> {
+  const LoginRoute({List<PageRouteInfo>? children})
+      : super(
+          LoginRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'LoginRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [RegisterPage]
+class RegisterRoute extends PageRouteInfo<void> {
+  const RegisterRoute({List<PageRouteInfo>? children})
+      : super(
+          RegisterRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'RegisterRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [RoomPage]
+class RoomRoute extends PageRouteInfo<void> {
+  const RoomRoute({List<PageRouteInfo>? children})
+      : super(
+          RoomRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'RoomRoute';
 
   static const PageInfo<void> page = PageInfo<void>(name);
 }
