45b70c70e6cacf7b6375735d3c09fc70b266feac
CAVALCANTE NOA p2107540 <noa.cavalcante@etu.univ-lyon1.fr>
2024-01-19 11:07:14 +0100

    Setup main

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,64 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hue_dada/auth/bloc/auth_cubit.dart';
+import 'package:hue_dada/auth/bloc/auth_state.dart';
+import 'package:hue_dada/home/repository/home_repository.dart';
 
+import 'firebase_options.dart';
 import 'navigation/app_router.dart';
 
-void main() {
-  runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FirebaseAuth.instance.signOut();
+
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
+class App extends StatelessWidget {
+  App({super.key});
 
   final _appRouter = AppRouter();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: _appRouter.config(),
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider<HomeRepository>(
+          create: (context) => HomeRepository(),
+        ),
+      ],
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider<AuthCubit>(
+            create: (context) => AuthCubit(),
+            child: Container(),
+          )
+        ],
+        child: BlocListener<AuthCubit, AuthState>(
+          listenWhen: (previous, current) => previous.user != current.user,
+          listener: (context, state) {
+            if (state.user == null) {
+              _appRouter.replace(const LoginRoute());
+            } else {
+              _appRouter.replace(const HomeRouterRoute());
+            }
+          },
+          child: MaterialApp.router(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              fontFamily: 'Sora',
+            ),
+            routerConfig: _appRouter.config(),
+          ),
+        ),
+      ),
     );
   }
 }
